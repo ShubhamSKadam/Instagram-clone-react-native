a4c714513fa995dd62cdf68f6f0e9474d1397e7c
Shubham.kadam <shubham.kadam@ip-10-1-42-49.ap-south-1.compute.internal>
2023-08-21 14:44:13 +0530

    setup bottom tabs

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./screens/HomeScreen";
+import SearchScreen from "./screens/SearchScreen";
+import PostScreen from "./screens/PostScreen";
+import ReelScreen from "./screens/ReelScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
+
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="home" component={HomeScreen}></Tab.Screen>
+        <Tab.Screen name="search" component={SearchScreen}></Tab.Screen>
+        <Tab.Screen name="post" component={PostScreen}></Tab.Screen>
+        <Tab.Screen name="reel" component={ReelScreen}></Tab.Screen>
+        <Tab.Screen name="profile" component={ProfileScreen}></Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
