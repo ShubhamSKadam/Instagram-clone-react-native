34779363ad4975331478f2eb6551184ce93b8828
Shubham.kadam <shubham.kadam@ip-10-1-42-49.ap-south-1.compute.internal>
2023-08-22 11:08:08 +0530

    update

---
+++ postsData.js
@@ -0,0 +1,24 @@
+export const postData = [
+  {
+    userId: 0,
+    post: {
+      postImage: require("./assets/PostImages/ssk.jpg"),
+      likes: 1210,
+    },
+    reel: {
+      reelVideo: "",
+      likes: 1500,
+    },
+  },
+  {
+    userId: 1,
+    post: {
+      postImage: require("./assets/PostImages/aditya.jpg"),
+      likes: 1210,
+    },
+    reel: {
+      reelVideo: "",
+      likes: 1500,
+    },
+  },
+];
