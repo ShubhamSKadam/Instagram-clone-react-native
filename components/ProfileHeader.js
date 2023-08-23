e767d65f2bf6b1593a88997b84774d33557e2610
Shubham.kadam <shubham.kadam@ip-10-1-42-49.ap-south-1.compute.internal>
2023-08-23 15:04:09 +0530

    profile nav modal styled

--- components/ProfileHeader.js
+++ components/ProfileHeader.js
@@ -6,12 +6,18 @@ import { Feather } from "@expo/vector-icons";
 import Modal from "react-native-modal";
 import { Fontisto } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const ProfileHeader = () => {
   const [isModalVisible, setModalVisible] = useState(false);
+  const [isHeaderModalVisible, setIsHeaderModalVisible] = useState(false);
 
   function modalHandler() {
     setModalVisible(!isModalVisible);
+  }
+
+  function headerModalhandler() {
+    setIsHeaderModalVisible(!isHeaderModalVisible);
   }
 
   return (
@@ -25,39 +31,96 @@ const ProfileHeader = () => {
 
         <View style={styles.createAdd}>
           <FontAwesome name="plus-square-o" size={28} color="black" />
-          <Feather name="menu" size={28} color="black" />
+          <TouchableOpacity onPress={headerModalhandler}>
+            <Feather name="menu" size={28} color="black" />
+          </TouchableOpacity>
         </View>
       </View>
 
-      {/* Modal Component */}
+      {/* Modal Component for userName */}
       <Modal
         isVisible={isModalVisible}
         testID={"modal"}
         onBackdropPress={() => setModalVisible(false)}
         style={styles.modal}
       >
-          <View style={styles.modalMainContainer}>
-            <View style={styles.modalUser}>
-              <Image
-                source={require("../assets/ProfileImages/shubham.png")}
-                style={styles.modalProfileImage}
-              />
-              <Text>ssk.exe</Text>
-            </View>
-
-            {/* Active account */}
-            <Fontisto name="radio-btn-active" size={24} color="#63bbdd" />
+        <View style={styles.modalMainContainer}>
+          <View style={styles.modalUser}>
+            <Image
+              source={require("../assets/ProfileImages/shubham.png")}
+              style={styles.modalProfileImage}
+            />
+            <Text>ssk.exe</Text>
           </View>
 
-          {/* Add Account */}
-          <View style={styles.addAccount}>
-            <View style={styles.addHighlightContainer}>
-              <View style={styles.addHighlight}>
-                <Ionicons name="add" size={32} color="black" />
-              </View>
+          {/* Active account */}
+          <Fontisto name="radio-btn-active" size={24} color="#63bbdd" />
+        </View>
+
+        {/* Add Account */}
+        <View style={styles.addAccount}>
+          <View style={styles.addHighlightContainer}>
+            <View style={styles.addHighlight}>
+              <Ionicons name="add" size={32} color="black" />
             </View>
-            <Text>Add account</Text>
           </View>
+          <Text>Add account</Text>
+        </View>
+      </Modal>
+
+      {/* Nav Header Modal */}
+      <Modal
+        isVisible={isHeaderModalVisible}
+        testID={"modal"}
+        onBackdropPress={() => setIsHeaderModalVisible(false)}
+        style={styles.modal}
+      >
+        <View style={styles.navHeaderModalContainer}>
+          <View style={styles.navHeaderModalItem}>
+            <Feather name="settings" size={24} color="black" />
+            <Text style={styles.navModalText}>Settings and privacy</Text>
+          </View>
+
+          <View style={styles.navHeaderModalItem}>
+            <MaterialIcons name="history" size={24} color="black" />
+            <Text style={styles.navModalText}>Your activity</Text>
+          </View>
+
+          <View style={styles.navHeaderModalItem}>
+            <Feather name="archive" size={24} color="black" />
+            <Text style={styles.navModalText}>Archive</Text>
+          </View>
+
+          <View style={styles.navHeaderModalItem}>
+            <FontAwesome name="qrcode" size={24} color="black" />
+            <Text style={styles.navModalText}>QR code</Text>
+          </View>
+
+          <View style={styles.navHeaderModalItem}>
+            <FontAwesome name="bookmark" size={24} color="black" />
+            <Text style={styles.navModalText}>Saved</Text>
+          </View>
+
+          <View style={styles.navHeaderModalItem}>
+            <Feather name="users" size={24} color="black" />
+            <Text style={styles.navModalText}>Supervision</Text>
+          </View>
+
+          <View style={styles.navHeaderModalItem}>
+            <MaterialIcons name="verified" size={24} color="black" />
+            <Text style={styles.navModalText}>Meta Verified</Text>
+          </View>
+
+          <View style={styles.navHeaderModalItem}>
+            <Fontisto name="nav-icon-list-a" size={20} color="black" />
+            <Text style={styles.navModalText}>Close Friends</Text>
+          </View>
+
+          <View style={styles.navHeaderModalItem}>
+            <Feather name="star" size={24} color="black" />
+            <Text style={styles.navModalText}>Favorites</Text>
+          </View>
+        </View>
       </Modal>
     </View>
   );
@@ -128,5 +191,22 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingHorizontal: 18,
     paddingBottom: 22,
+  },
+  // nav header modal stylings
+  navHeaderModalContainer: {
+    flexDirection: "column",
+    backgroundColor: "white",
+    paddingVertical: 14,
+    paddingHorizontal: 8,
+  },
+  navHeaderModalItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 14,
+    paddingHorizontal: 18,
+  },
+  navModalText: {
+    fontSize: 18,
+    paddingLeft: 14,
   },
 });
