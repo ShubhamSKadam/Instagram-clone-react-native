d7ba55d6839f7dbbdfad13cb7786102d944e064c
Shubham.kadam <shubham.kadam@ip-10-1-42-49.ap-south-1.compute.internal>
2023-08-23 15:10:35 +0530

    App.js screens modified to add new stack screen

--- App.js
+++ App.js
@@ -17,79 +17,92 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 export default function App() {
+  function BottomTabScreens() {
+    return (
+      <Tab.Navigator
+        screenOptions={{ headerShown: false, tabBarShowLabel: false }}
+      >
+        <Tab.Screen
+          name="home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size, focused }) => (
+              <Ionicons
+                name={focused ? "home" : "home-outline"}
+                size={28}
+                color="black"
+              />
+            ),
+          }}
+        ></Tab.Screen>
+        <Tab.Screen
+          name="search"
+          component={SearchScreen}
+          options={{
+            tabBarIcon: ({ color, size, focused }) => (
+              <Ionicons
+                name={focused ? "search-sharp" : "search-outline"}
+                size={28}
+                color="black"
+              />
+            ),
+          }}
+        ></Tab.Screen>
+        <Tab.Screen
+          name="post"
+          component={PostScreen}
+          options={{
+            tabBarIcon: ({ color, size, focused }) => (
+              <Octicons
+                name="diff-added"
+                size={28}
+                color={focused ? "gray" : "black"}
+              />
+            ),
+          }}
+        ></Tab.Screen>
+        <Tab.Screen
+          name="reel"
+          component={ReelScreen}
+          options={{
+            tabBarIcon: ({ color, size, focused }) => (
+              <MaterialCommunityIcons
+                name={focused ? "play-box" : "play-box-outline"}
+                size={28}
+                color="black"
+              />
+            ),
+          }}
+        ></Tab.Screen>
+        <Tab.Screen
+          name="profile"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({ color, size, focused }) => (
+              <FontAwesome
+                name={focused ? "user" : "user-o"}
+                size={28}
+                color="black"
+              />
+            ),
+          }}
+        ></Tab.Screen>
+      </Tab.Navigator>
+    );
+  }
+
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={{ headerShown: false, tabBarShowLabel: false }}
-        >
-          <Tab.Screen
-            name="home"
-            component={HomeScreen}
-            options={{
-              tabBarIcon: ({ color, size, focused }) => (
-                <Ionicons
-                  name={focused ? "home" : "home-outline"}
-                  size={28}
-                  color="black"
-                />
-              ),
-            }}
-          ></Tab.Screen>
-          <Tab.Screen
-            name="search"
-            component={SearchScreen}
-            options={{
-              tabBarIcon: ({ color, size, focused }) => (
-                <Ionicons
-                  name={focused ? "search-sharp" : "search-outline"}
-                  size={28}
-                  color="black"
-                />
-              ),
-            }}
-          ></Tab.Screen>
-          <Tab.Screen
-            name="post"
-            component={PostScreen}
-            options={{
-              tabBarIcon: ({ color, size, focused }) => (
-                <Octicons
-                  name="diff-added"
-                  size={28}
-                  color={focused ? "gray" : "black"}
-                />
-              ),
-            }}
-          ></Tab.Screen>
-          <Tab.Screen
-            name="reel"
-            component={ReelScreen}
-            options={{
-              tabBarIcon: ({ color, size, focused }) => (
-                <MaterialCommunityIcons
-                  name={focused ? "play-box" : "play-box-outline"}
-                  size={28}
-                  color="black"
-                />
-              ),
-            }}
-          ></Tab.Screen>
-          <Tab.Screen
-            name="profile"
-            component={ProfileScreen}
-            options={{
-              tabBarIcon: ({ color, size, focused }) => (
-                <FontAwesome
-                  name={focused ? "user" : "user-o"}
-                  size={28}
-                  color="black"
-                />
-              ),
-            }}
-          ></Tab.Screen>
-        </Tab.Navigator>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="BottomTabScreens"
+            component={BottomTabScreens}
+            options={{ headerShown: false }}
+          ></Stack.Screen>
+        </Stack.Navigator>
       </NavigationContainer>
     </Provider>
   );
