3dac0490a1cfbe0f19185f2c1408f1b03be71c63
Shubham.kadam <shubham.kadam@ip-10-1-42-49.ap-south-1.compute.internal>
2023-08-25 15:51:26 +0530

    last update post screen

--- screens/PostScreen.tsx
+++ screens/PostScreen.tsx
@@ -1,14 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 const PostScreen = () => {
+  const [image, setImage] = useState(null);
+
+  const openCamera = async () => {
+    try {
+      const result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.cancelled) {
+        setImage(result.uri);
+      }
+    } catch (error) {
+      console.error("Error opening camera:", error);
+    }
+  };
+
   return (
-    <View>
-      <Text>PostScreen</Text>
+    <View style={styles.container}>
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+      <TouchableOpacity onPress={openCamera}>
+        <Text>CAMERA ROLL</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default PostScreen
+export default PostScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
